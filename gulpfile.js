bb1dc5f0b41e9a477afb15d0c8b311a00690d4e3
[Jim Carroll] <[jim@pulamusic.com]>
2018-01-08 11:42:09 -0500

    added gulpfile and CSS files

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const gulp = require('gulp')
+const watch = require('gulp-watch')
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
+const cssvars = require('postcss-simple-vars')
+const nested = require('postcss-nested')
+
+gulp.task('default', function () {
+  console.log('You created a gulp task.')
+})
+
+gulp.task('html', function () {
+  console.log('Imagine something useful being done here.')
+})
+
+gulp.task('styles', function () {
+  return gulp.src('./app/assets/styles/styles.css')
+  .pipe(postcss([cssvars, nested, autoprefixer]))
+  .pipe(gulp.dest('./app/temp/styles'))
+})
+
+gulp.task('watch', function () {
+  watch('./app/index.html', function () {
+    gulp.start('html')
+  })
+
+  watch('./app/assets/styles/**/*.css', function () {
+    gulp.start('styles')
+  })
+})
