d1ba2c15a3de6c00c90fc92dd0e8d584d86b52ec
[Jim Carroll] <[jim@pulamusic.com]>
2018-01-11 11:37:56 -0500

    finished the nav menu

--- app/temp/scripts/app.js
+++ app/temp/scripts/app.js
@@ -103,6 +103,7 @@ var MobileMenu = function () {
   function MobileMenu() {
     _classCallCheck(this, MobileMenu);
 
+    this.siteHeader = (0, _jquery2.default)('.site-header');
     this.menuIcon = (0, _jquery2.default)('.site-header__menu-icon');
     this.menuContent = (0, _jquery2.default)('.site-header__menu-content');
     this.events();
@@ -116,7 +117,9 @@ var MobileMenu = function () {
   }, {
     key: 'toggleTheMenu',
     value: function toggleTheMenu() {
-      this.menuContent.toggleClass('.site-header__menu-content--is-visible');
+      this.menuContent.toggleClass('site-header__menu-content--is-visible');
+      this.siteHeader.toggleClass('site-header--is-expanded');
+      this.menuIcon.toggleClass('site-header__menu-icon--close-x');
     }
   }]);
 
